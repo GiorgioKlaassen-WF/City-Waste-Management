bd44d671e2d877937102268424ab369717ee192b
Giorgio Klaassen <giorgio.klaassen@windesheim.nl>
2022-05-20 10:02:34 +0200

    Added correct app files
    Added correct app files

--- CityWasteManagement/appcontainer/urls.py
+++ CityWasteManagement/appcontainer/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name="index"),
+    path('index/', views.index, name="index"),
     path('', views.index, name="index"),
+    path('index/humidity', views.index_humidity, name="index/humidity"),
+    path('locations/', views.locations, name="locations"),
+    path('future/', views.future, name="future"),
 ]
